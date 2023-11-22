15919ca58925dc4700546bdbfcd30ed71bfc86b0
Karan Kumar <kumarkaran02175@gmail.com>
2023-11-22 21:10:28 +0530

    changes by goel

--- script.js
+++ script.js
@@ -3,38 +3,38 @@ const navLinks = document.querySelectorAll('a[href^="#"]');
 
 // Add click event listener to each link
 navLinks.forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
 
-        // Get the target section's id from the href attribute
-        const targetId = this.getAttribute('href').substring(1);
+    // Get the target section's id from the href attribute
+    const targetId = this.getAttribute('href').substring(1);
 
-        // Find the target section based on its id
-        const targetSection = document.getElementById(targetId);
+    // Find the target section based on its id
+    const targetSection = document.getElementById(targetId);
 
-        // Scroll smoothly to the target section
-        targetSection.scrollIntoView({
-            behavior: 'smooth'
-        });
+    // Scroll smoothly to the target section
+    targetSection.scrollIntoView({
+      behavior: 'smooth'
     });
+  });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Initialize ScrollReveal with global options
-    const sr = ScrollReveal({
-      duration: 2000,      // Animation duration in milliseconds
-      reset: true,         // Reset the animation on scrolling back up
-      distance: '20px',    // Animation distance
-      origin: 'bottom',    // Animation origin (you can customize this)
-      easing: 'cubic-bezier(0.5, 0, 0, 1)', // Animation easing
-    });
-  
-    // Add animations to specific elements
-    sr.reveal(".get-in-touch", {
-      origin: "bottom",
-    });
+  // Initialize ScrollReveal with global options
+  const sr = ScrollReveal({
+    duration: 2000,      // Animation duration in milliseconds
+    reset: true,         // Reset the animation on scrolling back up
+    distance: '20px',    // Animation distance
+    origin: 'bottom',    // Animation origin (you can customize this)
+    easing: 'cubic-bezier(0.5, 0, 0, 1)', // Animation easing
+  });
 
-    // Example: Animate a section with the class "about-section"
+  // Add animations to specific elements
+  sr.reveal(".get-in-touch", {
+    origin: "bottom",
+  });
+
+  // Example: Animate a section with the class "about-section"
   sr.reveal(".about", {
     origin: "bottom",
   });
@@ -48,20 +48,20 @@ document.addEventListener("DOMContentLoaded", function () {
   sr.reveal(".experience", {
     origin: "bottom",
   });
-  
+
   // Example: Animate a section with the class "skills-section"
   sr.reveal(".pro", {
     origin: "bottom",
   });
-  
-    // You can add more elements here with specific animation options
-  
-    // Example: Animate a section with the class "example-section"
-    sr.reveal(".example-section", {
-      origin: "left",
-    });
+
+  // You can add more elements here with specific animation options
+
+  // Example: Animate a section with the class "example-section"
+  sr.reveal(".example-section", {
+    origin: "left",
   });
-  
+});
+
 // Animations for the skills section
 // Get the skills elements by class name
 const skills = document.querySelectorAll(".skill");
@@ -132,5 +132,22 @@ skillsSection.addEventListener("mouseout", () => {
 });
 
 // Animations for the footer section
+const nl = document.getElementById('navLinks')
+const humburger = document.querySelector('.humburger')
+const humLineEl = document.querySelectorAll('.humLine')
+let toggle = true
 
+humburger.addEventListener('click', () => {
+  if (toggle) {
+    nl.style.left = 0
 
+  }
+  else {
+    nl.style.left = '-100%'
+  }
+  toggle = !toggle;
+
+  humLineEl.forEach(item => {
+    item.classList.toggle('hb')
+  })
+})
