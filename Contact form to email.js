de5278155f91d08373cbdf808188a67eb93cd4a9
Karan Kumar <kumarkaran02175@gmail.com>
2024-02-10 23:31:42 +0530

    kk new changes

--- Contact form to email.js
+++ Contact form to email.js
@@ -1,33 +1,33 @@
-function emailSend(){
+function emailSend() {
 
-var userName = document.getElementById('name').value;
-var phone = document.getElementById('phone').value;
-var email = document.getElementById('email').value;
-var subject = document.getElementById('subject').value;
-var message = document.getElementById('message').value;
+    var userName = document.getElementById('name').value;
+    var phone = document.getElementById('phone').value;
+    var email = document.getElementById('email').value;
+    var subject = document.getElementById('subject').value;
+    var message = document.getElementById('message').value;
 
-var messageBody = "Name: " + userName +
-    "<br/> Phone: " + phone +
-    "<br/> Email: " + email +
-    "<br/> Subject: " + subject +
-    "<br/> Message: " + message;
-	Email.send({
-    Host : "smtp.elasticemail.com",
-    Username : "kumarkaran02175@gmail.com",
-    Password : "1790A6E427424EDD9235964CEDFAD74DD2CA",
-    To : 'dhurvkaran81@gmail.com',
-    From : "kumarkaran02175@gmail.com",
-    Subject : "New message from portfolio",
-    Body : messageBody
-}).then(
-    message => {
-        if(message=='OK'){
-            swal("Successful", "Thank you for getting in touch with me. Your message has been successfully received. I will review your message and get back to you as soon as possible.", "success")
-          
+    var messageBody = "Name: " + userName +
+        "<br/> Phone: " + phone +
+        "<br/> Email: " + email +
+        "<br/> Subject: " + subject +
+        "<br/> Message: " + message;
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "kkwebtesting@gmail.com",
+        Password: "94EB955FFAA2B0D79B75308AFA43A4F1D674",
+        To: 'kumarkaran02175@gmail.com',
+        From: "kkwebtesting@gmail.com",
+        Subject: "New message from portfolio",
+        Body: messageBody
+    }).then(
+        message => {
+            if (message == 'OK') {
+                swal("Successful", "Thank you for getting in touch with me. Your message has been successfully received. I will review your message and get back to you as soon as possible.", "success")
+
+            }
+            else {
+                swal("Error", "Oops! Something went wrong while processing your request. If the problem persists, you can contact me through other means.", "error")
+            }
         }
-        else{
-            swal("Error", "Oops! Something went wrong while processing your request. If the problem persists, you can contact me through other means.", "error") 
-        }
-    }
-  );
-  }
+    );
+}
--- script.js
+++ script.js
@@ -1,40 +1,50 @@
+// for progress bar
+const progressBar = document.getElementById("progressBar");
+window.addEventListener("scroll", () => {
+  const scrollPercentage =
+    (window.scrollY /
+      (document.documentElement.scrollHeight - window.innerHeight)) *
+    100;
+  progressBar.style.height = `${scrollPercentage}%`;
+});
+
 // Select all links with href starting with '#'
 const navLinks = document.querySelectorAll('a[href^="#"]');
 
 // Add click event listener to each link
 navLinks.forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
 
-        // Get the target section's id from the href attribute
-        const targetId = this.getAttribute('href').substring(1);
+    // Get the target section's id from the href attribute
+    const targetId = this.getAttribute('href').substring(1);
 
-        // Find the target section based on its id
-        const targetSection = document.getElementById(targetId);
+    // Find the target section based on its id
+    const targetSection = document.getElementById(targetId);
 
-        // Scroll smoothly to the target section
-        targetSection.scrollIntoView({
-            behavior: 'smooth'
-        });
+    // Scroll smoothly to the target section
+    targetSection.scrollIntoView({
+      behavior: 'smooth'
     });
+  });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Initialize ScrollReveal with global options
-    const sr = ScrollReveal({
-      duration: 2000,      // Animation duration in milliseconds
-      reset: true,         // Reset the animation on scrolling back up
-      distance: '20px',    // Animation distance
-      origin: 'bottom',    // Animation origin (you can customize this)
-      easing: 'cubic-bezier(0.5, 0, 0, 1)', // Animation easing
-    });
-  
-    // Add animations to specific elements
-    sr.reveal(".get-in-touch", {
-      origin: "bottom",
-    });
+  // Initialize ScrollReveal with global options
+  const sr = ScrollReveal({
+    duration: 2000,      // Animation duration in milliseconds
+    reset: true,         // Reset the animation on scrolling back up
+    distance: '20px',    // Animation distance
+    origin: 'bottom',    // Animation origin (you can customize this)
+    easing: 'cubic-bezier(0.5, 0, 0, 1)', // Animation easing
+  });
 
-    // Example: Animate a section with the class "about-section"
+  // Add animations to specific elements
+  sr.reveal(".get-in-touch", {
+    origin: "bottom",
+  });
+
+  // Example: Animate a section with the class "about-section"
   sr.reveal(".about", {
     origin: "bottom",
   });
@@ -48,75 +58,22 @@ document.addEventListener("DOMContentLoaded", function () {
   sr.reveal(".experience", {
     origin: "bottom",
   });
-  
+
   // Example: Animate a section with the class "skills-section"
   sr.reveal(".pro", {
     origin: "bottom",
   });
-  
-    // You can add more elements here with specific animation options
-  
-    // Example: Animate a section with the class "example-section"
-    sr.reveal(".example-section", {
-      origin: "left",
-    });
-  });
-  
-// Animations for the skills section
-// Get the skills elements by class name
-const skills = document.querySelectorAll(".skill");
 
-// Create a GSAP timeline for the skills animation
-const skillsTl = gsap.timeline({ paused: true }); // Pause the timeline initially
+  // You can add more elements here with specific animation options
 
-// Loop through each skill element and add animations
-skills.forEach((skill, index) => {
-  skillsTl.from(skill, {
-    opacity: 0,
-    x: -100,
-    duration: 0.5,
-    delay: index * 0.2, // Delay each skill's animation
-  });
-
-  const text = skill.querySelector("span");
-
-  skill.addEventListener("mouseenter", () => {
-    gsap.to(text, {
-      x: 0,
-      y: 0,
-      duration: 0.2,
-      repeat: 5,
-      yoyo: true,
-    });
-  });
-
-  skill.addEventListener("mouseout", () => {
-    gsap.to(text, {
-      x: 0,
-      y: 0,
-      duration: 0.2,
-    });
-  });
-
-  skill.addEventListener("mousemove", (event) => {
-    const rect = skill.getBoundingClientRect();
-
-    const relX = event.clientX - rect.left - rect.width / 2;
-    const relY = event.clientY - rect.top - rect.height / 2;
-
-    const angle = Math.atan2(relY, relX);
-    const maxRadius = (rect.width / 2) - (text.offsetWidth / 2);
-
-    const x = maxRadius * Math.cos(angle);
-    const y = maxRadius * Math.sin(angle);
-
-    gsap.to(text, {
-      x: x,
-      y: y,
-      duration: 0.2,
-    });
+  // Example: Animate a section with the class "example-section"
+  sr.reveal(".example-section", {
+    origin: "left",
   });
 });
+
+// Animations for the skills section
+
 
 // Get the skills section by its ID
 const skillsSection = document.getElementById("Skills");
@@ -132,5 +89,36 @@ skillsSection.addEventListener("mouseout", () => {
 });
 
 // Animations for the footer section
+const nl = document.getElementById('navLinks')
+const humburger = document.querySelector('.humburger')
+const humLineEl = document.querySelectorAll('.humLine')
+let toggle = true
 
+humburger.addEventListener('click', () => {
+  if (toggle) {
+    nl.style.left = 0
 
+  }
+  else {
+    nl.style.left = '-100%'
+  }
+  toggle = !toggle;
+
+  humLineEl.forEach(item => {
+    item.classList.toggle('hb')
+  })
+})
+
+// for project section click on me 
+function moveText(event) {
+  const card = event.currentTarget;
+  const hoverText = card.querySelector('.hover-text');
+
+  // Calculate relative position within the card
+  const x = event.clientX - card.getBoundingClientRect().left;
+  const y = event.clientY - card.getBoundingClientRect().top;
+
+  // Update the position of the hover text
+  hoverText.style.left = `${x}px`;
+  hoverText.style.top = `${y}px`;
+}
